17573f4bf466feb5cf243798be0f5159a1bd9958
juanschcolnik <schcolnikj@gmail.com>
2023-11-16 12:08:18 -0300

    Formik validations added over SignUp and SignIn

--- src/pages/auth/sign-in/SignInPage.tsx
+++ src/pages/auth/sign-in/SignInPage.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import logo from "../../../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -8,22 +7,33 @@ import LabeledInput from "../../../components/labeled-input/LabeledInput";
 import Button from "../../../components/button/Button";
 import { ButtonType } from "../../../components/button/StyledButton";
 import { StyledH3 } from "../../../components/common/text";
+import { useFormik } from "formik";
+import { signInSchema } from "../../../schemas";
+import { SingInData } from "../../../service";
 
 const SignInPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-
   const httpRequestService = useHttpRequestService();
   const navigate = useNavigate();
   const { t } = useTranslation();
 
-  const handleSubmit = () => {
-    httpRequestService
-      .signIn({ email, password })
-      .then(() => navigate("/"))
-      .catch(() => setError(true));
+  const onSubmit = async (values: SingInData) => {
+    try {
+      const data = {username: values.username, password: values.password}
+      await httpRequestService.signIn(data)
+      navigate("/")
+    } catch (error) {
+      navigate('/sign-in');
+    }
   };
+
+  const { values, errors, handleChange, handleSubmit, touched } = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: onSubmit,
+    validationSchema: signInSchema,
+  })
 
   return (
     <AuthWrapper>
@@ -33,30 +43,38 @@ const SignInPage = () => {
             <img src={logo} alt={"Twitter Logo"} />
             <StyledH3>{t("title.login")}</StyledH3>
           </div>
-          <div className={"input-container"}>
+          <form onSubmit={handleSubmit} className={"input-container"}>
             <LabeledInput
               required
               placeholder={"Enter user..."}
               title={t("input-params.username")}
-              error={error}
-              onChange={(e) => setEmail(e.target.value)}
+              name="username"
+              id="username"
+              onChange={handleChange("username")}
+              value={values.username}
             />
+            {errors.username && <span>{errors.username}</span>}
+            
             <LabeledInput
               type="password"
               required
               placeholder={"Enter password..."}
               title={t("input-params.password")}
-              error={error}
-              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              id="password"
+              onChange={handleChange("password")}
+              value={values.password}
             />
-            <p className={"error-message"}>{error && t("error.login")}</p>
-          </div>
+            {errors.password && touched.password && <span>{errors.password}</span>}
+
+            <p className={"error-message"}>{errors && touched && t("error.login")}</p>
+          </form>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <Button
               text={t("buttons.login")}
               buttonType={ButtonType.FOLLOW}
               size={"MEDIUM"}
-              onClick={handleSubmit}
+              onClick={() => handleSubmit()}
             />
             <Button
               text={t("buttons.register")}
